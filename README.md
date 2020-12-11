bffbf922d20f04469133904bcc81bcd7e3ff8f84
Travis Ko <tkospeaks@gmail.com>
2020-12-10 17:39:27 -0800

    Including fixed code for all pop ups to successfully deploy. Also including screenshot of deployed application, updated README with new specifics. Updated comments within code to make things easier to understand. Semantically lined up all elements to conform to standards. Created multiple if loops based on storing results of chosen characters, numbers and letters. Updated javascript to properly store the results of chose prompts.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Portfolio-Password-Generator
-This is a repository dedicated to generating secure and encrypted passwords.
+Password Generator Read Me
+
+This is a project to deploy a functioning password generator that is utilizable up to 128 characters, using upper case, lower case, numbers, and special characters.
+
+
+It follows these rules:
+Click a button to generate a new password, and it presents a series of prompts for password criteria.
+These include upper case, lower case, numbers and special characters.
+A prompt for length of password follows, choose a length of at least 8 characters and no more than 128.
+Following this a prompt pops up for character types to include in password, choose lowercase, uppercase, numeric, and/or special characters.
+Based on the criteria chosen, a password is generated that matches all criteria defined in previous inquiries.
+Display password written on the page.
+
+Here is a link to the published page: https://techkospeaks.github.io/Portfolio-Password-Generator/ 
+
+Screenshot is containted within documents as well to deployed application.
+
+
+
+
+## Review
+
+You are required to submit the following for review:
+
+* The URL of the deployed application.
+
+* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+- - -
+© 2020 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- script.js
+++ script.js
@@ -1,17 +1,86 @@
-// Assignment Code
+
+// Defining the variables for each of the elements
 var generateBtn = document.querySelector("#generate");
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers = "0123456789".split("");
+var characters = "~!@#$%^&*()_+:?><;[]=-".split("");
+
+// Creating an array to hold the password and an array to store the variables of password in a pool
+function generatePassword() {
+    var password = [];
+    var pool = [];
+
+// Ask user to enter password length
+
+    var passLength = prompt("Please choose a password character length, between 7 - 128"); 
+    passLength = parseInt(passLength); 
+
+// If user has entered less than 8 or more than 128 characters an alert pops up asking to keep it within those parameters
+
+    if (passLength < 8) {
+        alert("Password must have more than 7 characters");
+        return "";
+    }
+    if (passLength > 128) {
+        alert("Password must not have more than 128 characters");
+        return "";
+    }
+
+// Inquiring if the user would like lowercase, numbers, upper case, and special characters
+
+    var useLowercase = confirm("Do you want lowercase letters in your password's life?");
+
+    var useNumbers = confirm("Need some numbers to keep things REAL secure?");
+
+    var useUppercase = confirm("How about some sumptuous UPPER CASE letters to spice up your password?")
+
+    var useCharacter = confirm("Do you want EVEN MORE security with special characters?")
+
+// If user has picked each of the options, pool them and copy to store each option in the pool as a whole (characters, uppercase, numbers and lowercase)
+
+    if (useLowercase) {
+        pool = pool.concat(lowercase);
+    }
+    if (useUppercase) {
+      pool = pool.concat(uppercase);
+  }
+    if (useNumbers) {
+        pool = pool.concat(numbers);
+    }
+    if (useCharacter) {
+      pool = pool.concat(characters);
+  }
+
+// Generate a random password based on the pool and randomization of characters and numbers
+
+    for (var i = 0; i < passLength; i++) {
+        var randomChar = pool[Math.floor(Math.random() * pool.length)];
+        password.push(randomChar);
+    }
+
+// Add to the end of the password space and returning to join all the components
+    return password.join("");
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-    
-
-  passwordText.value = password;
-
-  prompt=("Please choose a password");
-
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Add event listener to generate the button for creating a password
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// If I click the button to generate a new password, present a series of prompts for password criteria
+// If prompt comes up for password criteria, select criteria to include in password
+// If prompt comes up for length of password, choose a length of at least 8 characters and no more than 128.
+// If prompt comes up for character types to include in password, choose lowercase, uppercase, numeric, and/or special characters
+// If I answer every prompt, input is validated and at least one character type should select
+// If all prompts answered, password generates that matches criteria
+//If password is generated, displayed in alert or written to page.
+
+
